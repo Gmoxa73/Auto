a06ac725b628b79a550216911616d19d8c3ace45
Dmitry <gmoxa@rambler.ru>
2025-10-23 19:09:02 +0300

    change test.py

--- aa/tests.py
+++ aa/tests.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
-
 from rest_framework.test import APIClient
 
 class MyTest(TestCase):
     def test_ok(self):
         self.assertTrue(True)
+
 
     def test_sample_view(self):
         url = "/list/"
