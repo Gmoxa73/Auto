a1256aa2514ca4ae2d58a95b862217d74aadf9c7
Dmitry <gmoxa@rambler.ru>
2025-10-23 19:07:34 +0300

    change serializer.py 9

--- aa/serializers.py
+++ aa/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Raions, Addresses, Devices, Types
 
+
 class RaionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Raions
